dd26628bd177718ecc785b8cb7c34402420ff0b8
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-06-15 14:12:00 +0530

    Add files via upload

---
+++ bin_oct_hex.py
@@ -0,0 +1,17 @@
+# print(bin(7))
+# print(0b111)
+# print(0B111)
+
+# print(oct(17))
+# print(0o21)
+# print(0O21)
+
+# print(hex(18))
+# print(0x12)
+# print(0X12)
+# print(0XA)
+# print(0Xb)
+# print(0Xc)
+# print(0Xd)
+# print(0Xe)
+# print(0Xf)
